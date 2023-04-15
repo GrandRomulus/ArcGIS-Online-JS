fe1a01921194169a5c0909d0b6b3ef87880c87f4
GrandRomulus <102898805+GrandRomulus@users.noreply.github.com>
2023-04-16 00:07:18 +0930

    Change prompt timing, location etc in code.

--- TOC_Layer_Change.js
+++ TOC_Layer_Change.js
@@ -7,15 +7,15 @@ function openRenameTextbox(layer){
 // Function that deletes input string which is passed into 'newName'
 function changeLayerString(list, newName) {
     const textbox = list.querySelector('.esri-input[data-node-ref="editInputNode"]');
-    const currentValue = textbox.value;
+    const currentValue = textbox.value.toLowerCase();;
     const index = currentValue.indexOf(newName);
     
     if (index !== -1) {
       const newValue = currentValue.slice(0, index) + currentValue.slice(index + newName.length);
       textbox.value = newValue;
-      alert("Layer change was successful.");
+      alert("Layer change for '" + currentValue + "' was successful.");
     } else {
-      alert("No match. The layer '" + currentValue + "' did not contain '" + newName + "'. Try again.");
+      alert("No match. The layer '" + currentValue + "' did not contain '" + newName + "'. Try again.")
     }
   }
 
@@ -28,6 +28,8 @@ function submitNewNameString(list){
 // Declaring layerList variable for forEach loop
 const layerList = document.querySelectorAll('.esri-layer-list__item');
 
+const newName = prompt("What part of the layer name would you like to delete?").toLowerCase();
+
 // Loop that loops through layer list to open all layer name inputs
 layerList.forEach((layer) => {
   openRenameTextbox(layer);
@@ -36,14 +38,13 @@ layerList.forEach((layer) => {
 // Delay declaration of variable to capture class values for layer name change and forEach loop
 setTimeout(() => {
   const inputList = document.querySelectorAll('.esri-title__authoring-form');
-  const newName = prompt("What part of the layer name would you like to delete?");
 
   // Loop that loops through input list to perform name change and submition
   inputList.forEach((list) => {
       changeLayerString(list, newName);
       submitNewNameString(list);
   });
-}, 2000); // Delay in milliseconds (2 seconds in this example)
+}, 1000); // Delay in milliseconds (2 seconds in this example)
 
 
 
