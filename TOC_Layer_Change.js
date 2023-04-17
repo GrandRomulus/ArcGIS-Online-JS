23fe0e022322c4f3bd07c2b1cce2ac685f5df26c
GrandRomulus <102898805+GrandRomulus@users.noreply.github.com>
2023-04-17 10:25:27 +0930

    Update TOC_Layer_Change.js

--- TOC_Layer_Change.js
+++ TOC_Layer_Change.js
@@ -15,14 +15,13 @@ function changeLayerString(list, newName) {
     const textbox = list.querySelector('.esri-input[data-node-ref="editInputNode"]');
     const currentValue = textbox.value;
     const index = currentValue.indexOf(newName);
-    
-    if (index !== -1) {
-      layersChanged += currentValue + '\n';
-      const newValue = currentValue.slice(0, index) + currentValue.slice(index + newName.length);
-      textbox.value = newValue;
-  };
-};
 
+    if (index !== -1) {
+        layersChanged += currentValue + '\n';
+        const newValue = currentValue.slice(0, index) + currentValue.slice(index + newName.length);
+        textbox.value = newValue;
+    }
+}
 
 // Function that submits what is in the textbox
 function submitNewNameString(list){
@@ -35,31 +34,30 @@ const layerList = document.querySelectorAll('.esri-layer-list__item');
 
 // Loop that loops through layer list to open all layer name inputs
 layerList.forEach((layer) => {
-  try {
-    openRenameTextbox(layer);
-  } catch (error) {
-    if (error instanceof TypeError) {
-      // Ignore TypeError and move on to the next iteration
-      return;
-    } else {
-      // Handle other types of errors as needed
-      console.error(error);
+    try {
+        openRenameTextbox(layer);
+    } catch (error) {
+        if (error instanceof TypeError) {
+            // Ignore TypeError and move on to the next iteration
+            return;
+        } else {
+            // Handle other types of errors as needed
+            console.error(error);
+        }
     }
-  }
 });
 
 // Delay declaration of variable to capture class values for layer name change and forEach loop
 setTimeout(() => {
-  const inputList = document.querySelectorAll('.esri-title__authoring-form');
+    const inputList = document.querySelectorAll('.esri-title__authoring-form');
 
-  // Loop that loops through input list to perform name change and submition
-  inputList.forEach((list) => {
-      changeLayerString(list, newName);
-      submitNewNameString(list);
-  });
-  alert("Layer changed:\n" + layersChanged)
+    // Loop that loops through input list to perform name change and submition
+    inputList.forEach((list) => {
+        changeLayerString(list, newName);
+        submitNewNameString(list);
+    });
+    alert("Layer changed:\n" + layersChanged)
 }, 1000); // Delay in milliseconds (2 seconds in this example)
-
     
     
     
