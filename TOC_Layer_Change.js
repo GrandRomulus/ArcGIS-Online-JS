6c3d4a99fd30ca10d5e44df9230c42ef346514bc
GrandRomulus <102898805+GrandRomulus@users.noreply.github.com>
2023-04-17 15:20:52 +0930

    Removed prompt and split on '-'.

--- TOC_Layer_Change.js
+++ TOC_Layer_Change.js
@@ -1,8 +1,5 @@
-// Declaring variables for layer change storage
+// Declaring variable for layer change storage
 let layersChanged = "";
-
-// Prompt to capture what users wants to change
-const newName = prompt("What part of the layer name would you like to delete?");
 
 // Function that opens 'Rename' menu for layer
 function openRenameTextbox(layer){
@@ -11,52 +8,49 @@ function openRenameTextbox(layer){
 }
 
 // Function that deletes input string which is passed into 'newName'
-function changeLayerString(list, newName) {
+function changeLayerString(list) {
     const textbox = list.querySelector('.esri-input[data-node-ref="editInputNode"]');
     const currentValue = textbox.value;
-    const index = currentValue.indexOf(newName);
+    layersChanged += currentValue + '\n';
+    const newValue = currentValue.split('-', 1);
+    textbox.value = newValue;
+};
 
-    if (index !== -1) {
-        layersChanged += currentValue + '\n';
-        const newValue = currentValue.slice(0, index) + currentValue.slice(index + newName.length);
-        textbox.value = newValue;
-    }
-}
 
 // Function that submits what is in the textbox
 function submitNewNameString(list){
     const okButton = list.querySelector('.esri-button[type="submit"]');
     okButton.click();
-}
+};
 
 // Declaring layerList variable for forEach loop
 const layerList = document.querySelectorAll('.esri-layer-list__item');
 
 // Loop that loops through layer list to open all layer name inputs
 layerList.forEach((layer) => {
-    try {
-        openRenameTextbox(layer);
-    } catch (error) {
-        if (error instanceof TypeError) {
-            // Ignore TypeError and move on to the next iteration
-            return;
-        } else {
-            // Handle other types of errors as needed
-            console.error(error);
-        }
+  try {
+    openRenameTextbox(layer);
+  } catch (error) {
+    if (error instanceof TypeError) {
+      // Ignore TypeError and move on to the next iteration
+      return;
+    } else {
+      // Handle other types of errors as needed
+      console.error(error);
     }
+  }
 });
 
 // Delay declaration of variable to capture class values for layer name change and forEach loop
 setTimeout(() => {
-    const inputList = document.querySelectorAll('.esri-title__authoring-form');
+  const inputList = document.querySelectorAll('.esri-title__authoring-form');
 
-    // Loop that loops through input list to perform name change and submition
-    inputList.forEach((list) => {
-        changeLayerString(list, newName);
-        submitNewNameString(list);
-    });
-    alert("Layer changed:\n" + layersChanged)
+  // Loop that loops through input list to perform name change and submition
+  inputList.forEach((list) => {
+      changeLayerString(list);
+      submitNewNameString(list);
+  });
+  alert("Layer changed:\n" + layersChanged);
 }, 1000); // Delay in milliseconds (2 seconds in this example)
     
     
